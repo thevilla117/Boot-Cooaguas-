3a936d80e63d0a0836e73b8ccb39011c63fc4ad8
thevilla117 <thevilla117@users.noreply.github.com>
2026-03-23 20:52:59 -0500

    Instalar dependencias clave de puppeteer y revertir paths

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || null,
         args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-gpu']
     }
 });
