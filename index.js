d69d293173f02acf78a4ef6033f0c7e6752ca711
thevilla117 <thevilla117@users.noreply.github.com>
2026-03-23 23:05:10 -0500

    Fix: remover webVersionCache y flags

--- index.js
+++ index.js
@@ -16,10 +16,6 @@ const isLinux = os.platform() === 'linux';
 
 const client = new Client({
     authStrategy: new LocalAuth(),
-    webVersionCache: {
-        type: 'remote',
-        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html'
-    },
     puppeteer: {
         headless: true,
         executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || null,
